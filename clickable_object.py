804160304a0cb2077e86cad1d9a1602ae548bf41
Ian Harris <ian.harris@fhu.students.edu>
2026-04-03 19:42:01 -0500

    Runs clickable_object.py smoother and better than before

--- clickable_object.py
+++ clickable_object.py
@@ -1,4 +1,4 @@
-class Clickable_Object:
+'''class Clickable_Object:
     def __init__(self, name, description):
         self.name = name
         self.description = description
@@ -13,5 +13,20 @@ class Clickable_Object:
         if self.usable_item():
             print(f'You picked up the {self.name} and added it to your inventory.')
             usable_item.append(self)
+        else:
+            print(f'You clicked on {self.name}. {self.description}')'''
+
+class ClickableObject:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def is_usable(self):
+        return False
+
+    def click(self, inventory):
+        if self.is_usable():
+            print(f'You picked up the {self.name} and added it to your inventory.')
+            inventory.append(self)
         else:
             print(f'You clicked on {self.name}. {self.description}')
