85b84bae0621b5d23568071d08b61745d4b87891
Ian Harris <ian.harris@fhu.students.edu>
2026-04-03 19:40:57 -0500

    Fixed the clickable_object.py to work smoother and be less confusing

--- clickable_object.py
+++ clickable_object.py
@@ -2,9 +2,16 @@ class Clickable_Object:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+    
+    def usable_item(self):
+        return False
+    
+    def inventory(self):
+        self.inventory = []
 
-    def click(self):
-        if usable_item:
+    def click(self, usable_item):
+        if self.usable_item():
             print(f'You picked up the {self.name} and added it to your inventory.')
+            usable_item.append(self)
         else:
             print(f'You clicked on {self.name}. {self.description}')
