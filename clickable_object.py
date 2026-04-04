80e0d1a020306a565b842e0489924250a0196bc1
Ian Harris <ian.harris@fhu.students.edu>
2026-04-03 21:59:39 -0500

    Edited the clickable_object.py file yet again

--- clickable_object.py
+++ clickable_object.py
@@ -15,18 +15,26 @@
             usable_item.append(self)
         else:
             print(f'You clicked on {self.name}. {self.description}')'''
-
+inventory = []
 class ClickableObject:
-    def __init__(self, name, description):
+    def __init__(self, name, description, usable):
         self.name = name
         self.description = description
+        self.usable = True if usable else False
 
-    def is_usable(self):
-        return False
 
     def click(self, inventory):
-        if self.is_usable():
+        if self.usable:
             print(f'You picked up the {self.name} and added it to your inventory.')
             inventory.append(self)
         else:
             print(f'You clicked on {self.name}. {self.description}')
+
+###########Inventory and testing code below###########
+screwdriver = ClickableObject('screwdriver', 'A tool used for turning screws.', True)
+door = ClickableObject('door', 'A sturdy wooden door. It seems to be locked.', False)
+print(screwdriver.description)
+print(door.description)
+screwdriver.click(inventory)
+door.click(inventory)
+print(f'Inventory: {[item.name for item in inventory]}')
